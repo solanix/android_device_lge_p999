e7f80cf8c57408aa1771c1de00a23a1b8be8201b
Benedikt Boss <boss.benedikt@googlemail.com>
2012-09-04 23:16:13 +0300

    Same as on p990. Should be p999 instead of lgp999

--- cm.mk
+++ cm.mk
@@ -18,4 +18,4 @@ TARGET_BOOTANIMATION_NAME := 480
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p999
 
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=lgp999 PRODUCT_NAME=lge_lge_star BUILD_ID=GRI40 BUILD_FINGERPRINT=lge/lge_lge_star/p999:2.3.3/GRI40/lgp999-V21e.41fdc8a2:user/release-keys PRIVATE_BUILD_DESC="lge_star-user 2.3.3 GRI40 41fdc8a2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=p999 PRODUCT_NAME=lge_lge_star BUILD_ID=GRI40 BUILD_FINGERPRINT=lge/lge_lge_star/p999:2.3.3/GRI40/lgp999-V21e.41fdc8a2:user/release-keys PRIVATE_BUILD_DESC="lge_star-user 2.3.3 GRI40 41fdc8a2 release-keys"
